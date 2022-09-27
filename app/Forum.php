6817034852b98ffeef1f63062cf204c1680a8132
MartaRondan96 <martarondan96@gmail.com>
2022-09-27 08:38:32 +0200

    Primer commit

---
+++ app/Forum.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Forum extends Model
+{
+    protected $table='forums';
+
+    protected $fillable=['id','name','description'];
+}
---
+++ app/Http/Controllers/ForumController.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Forum;
+
+class ForumController extends Controller
+{
+    public function index(){
+      //Utiliza el modelo forum
+      //Muestra todos los registros de la tabla forum
+      //Es como un SELECT * FROM forum;
+		//$forums=Forum::all();
+      $forums = Forum ::latest()->paginate(5);
+        //Muestra un registro de la BD
+  //  dd($forums[3]);
+        //Mostrar todos los registros de la BD
+       // dd($forums);
+
+    return view('forums.index', compact('forums'));
+	}
+}
