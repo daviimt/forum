630bbd2ab9fe8af7f5784d87b544735feff8cbde
daviimt <dmateomerino@gmail.com>
2022-10-05 08:51:53 +0200

    postcontroller

--- app/Http/Controllers/PostController.php
+++ app/Http/Controllers/PostController.php
@@ -18,7 +18,7 @@ class PostController extends Controller
 
 	public function store(PostRequest $post_request) 
 	{
-		
+		$post_request->merge(['user_id' => auth()->id()]);
 		Post::create($post_request->input()); // Esto coge todos los datos que vienen vía Post y los inserta
 		return back()->with('message', ['success', __('Post creado correctamente')]);
 	
