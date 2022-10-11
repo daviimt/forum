7dc4d43b5c281a0ec2033afaf3a19b5f32454ea4
daviimt <dmateomerino@gmail.com>
2022-10-11 08:20:40 +0200

    Formulario para crear nuevos post

--- app/Post.php
+++ app/Post.php
@@ -3,12 +3,25 @@
 namespace App;
 
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Facades\App;
 
 class Post extends Model
 {
     protected $table='posts';
     protected $fillable=['id','user_id','forum_id','title','description'];
     //'forum_id es la clave foranea de la tabla post'
+
+    protected static function boot() {
+        parent::boot();
+
+        static::creating(function($post) {
+            if( ! App::runningInConsole() ) {
+            $post->user_id = auth()->id();
+            }
+        });
+    }
+
+
     public function forum(){
     	return $this->belongsTo(Forum::class, 'forum_id');
     }
